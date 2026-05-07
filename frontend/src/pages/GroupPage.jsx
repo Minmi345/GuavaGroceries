c09a63b1281bac073265e85212711f6007f68074
Dechen Dolkar <dechendolkar@MacBookAir.lan>
2026-05-08 01:19:56 +0200

    added new pages, created api.js for frontend backend communication and it now works as a pwa

---
+++ frontend/src/pages/GroupPage.jsx
@@ -0,0 +1,8 @@
+export default function GroupPage() {
+    return (
+      <div className="p-8">
+        <h1 className="text-2xl font-bold">Group</h1>
+        <p>Group page coming soon</p>
+      </div>
+    )
+  }
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,17 +3,20 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
     VitePWA({
       registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
       manifest: {
         name: 'GuavaGroceries',
         short_name: 'GuavaGroceries',
-        description: 'Smart receipt scanning and budget tracking', 
+        description: 'Smart receipt scanning and budget tracking',
         theme_color: '#ffffff',
         display: 'standalone',
         orientation: 'portrait',
@@ -32,7 +35,6 @@ export default defineConfig({
         ]
       }
     })
-  
   ],
-  
 })
+
