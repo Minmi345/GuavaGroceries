44603d57c22bda8f1613897923689ce8a98eae6e
Dechen Dolkar <dechendolkar@MacBookAir.lan>
2026-05-07 22:51:53 +0200

    configured pwa plugin

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,38 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'GuavaGroceries',
+        short_name: 'GuavaGroceries',
+        description: 'Smart receipt scanning and budget tracking', 
+        theme_color: '#ffffff',
+        display: 'standalone',
+        orientation: 'portrait',
+        start_url: '/',
+        icons: [
+          {
+            src: 'icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  
+  ],
   
 })
