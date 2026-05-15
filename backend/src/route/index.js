e862f113552a1fbfcc96bc6cc3655a96934dcda3
Rebecca Blixt <rebecca.blixt@hotmail.com>
2026-05-15 15:18:13 +0200

    fix spelling error

--- backend/src/route/index.js
+++ backend/src/route/index.js
@@ -3,16 +3,12 @@ import { router as helloRoute } from './hello.js'
 import { router as userRoute } from './users-route.js'
 import { router as jwtRoute } from './jwt-route.js'
 import { router as ocrRoute } from './ocr-route.js'
-import { router as recriptRoute } from './receipt-route.js'
+import { router as receiptRoute } from './receipt-route.js'
 
 export const router = express.Router()
 
 router.use('/', express.json(), helloRoute)
 router.use('/users', express.json(), userRoute)
 router.use('/jwt', express.json(), jwtRoute)
-router.use('/receipts', express.json(), recriptRoute)
+router.use('/receipts', express.json(), receiptRoute)
 router.use('/ocr', ocrRoute)
-
-
-
-
